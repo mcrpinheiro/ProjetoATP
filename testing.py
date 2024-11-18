3d19664fdea0a65b55443a7dfa96c8dfd2f16f96
mcrpinheiro <mcardosorp@gmail.com>
2024-11-18 13:12:46 +0000

    adding tests

---
+++ testing.py
@@ -0,0 +1,55 @@
+
+import json
+ata = {       "abstract": "Resumo Introdução: A perturbação esquizofreniforme manifesta-se com sintomas semelhantes aos da esquizofrenia, mas distingue-se desta pela sua duração mais curta, que varia entre um e seis meses. O objetivo deste estudo foi descrever e analisar as hospitalizações por perturbação esquizofreniforme numa base de dados nacional de internamentos.Métodos: Planeámos um estudo retrospetivo, utilizando uma base de dados de hospitalizações a nível nacional, contendo todos os internamentos registados em hospitais públicos de Portugal Continental entre 2008 e 2015. As hospitalizações com diagnóstico primário de perturbação esquizofreniforme foram selecionadas com base no código de diagnóstico 295.4x da Classificação Internacional de Doenças versão 9, Modificação Clínica (CID-9-CM). Foram obtidos dados relativos à data de nascimento, ao sexo, à zona de residência, aos diagnósticos, à duração do internamento (length of stay), ao estado de alta e às despesas hospitalares. As comorbilidades foram analisadas com recurso à pontuação do índice de Charlson. Foram efetuados testes t para amostras independentes para avaliar diferenças em variáveis contínuas com distribuição normal e testes Mann-Whitney-U quando esta distribuição não foi registada.Resultados: Nos hospitais públicos de Portugal Continental, ocorreu um total de 594 internamentos com o diagnóstico primário de perturbação esquizofreniforme durante o período de oito anos selecionado. A maioria, 72,1% (n = 428), estava associada a doentes do sexo masculino. A idade média de admissão foi de 34,34 anos nos homens e de 40,19 anos nas mulheres. A mediana do length of stay foi de 17,00 dias e a mortalidade intra-hospitalar foi de 0,5% (n = 3). Apenas 6,1% (n = 36) dos episódios de internamento tinham uma ou mais comorbilidades registadas. Foram documentados 41 reinternamentos.Conclusão: As hospitalizações com o diagnóstico principal de perturbação esquizofreniforme ocorrem mais frequentemente em doentes jovens do sexo masculino. Tanto quanto é do conhecimento dos autores, este é o primeiro estudo de âmbito nacional que analisa todos os internamentos por este diagnóstico em Portugal.",
+        "keywords": "Hospitalização, Perturbações Psicóticas",
+        "authors": [
+            {
+                "name": "Inês Teixeira da Cunha",
+                "affiliation": "Faculty of Medicine. Universidade do Porto. Porto."
+            },
+            {
+                "name": "Celeste Silveira",
+                "affiliation": "Psychiatry Service. Unidade Local de Saúde São João. Porto."
+            },
+            {
+                "name": "Alberto Freitas",
+                "affiliation": "CINTESIS@RISE. Department of Community Medicine, Information and Health Decision Sciences (MEDCIDS). Faculty of Medicine. Universidade do Porto. Porto."
+            },
+            {
+                "name": "Manuel Gonçalves Pinho",
+                "affiliation": "CINTESIS@RISE. Department of Clinical Neurosciences and Mental Health. Faculty of Medicine. Universidade do Porto. Porto; Department of Psychiatry and Mental Health. Unidade Local de Saúde Tâmega e Sousa. Penafiel."
+            }
+        ],
+        "doi": "https://doi.org/10.20344/amp.21714",
+        "pdf": "https://www.actamedicaportuguesa.com/revista/index.php/amp/article/view/21714/15498",
+        "publish_date": "2024-09-06",
+        "title": "Hospitalizações por Perturbação Esquizofreniforme: Uma Análise Clínica e Demográfica de uma Base de Dados Nacional",
+        "url": "https://www.actamedicaportuguesa.com/revista/index.php/amp/article/view/21714"
+    }
+
+def carregarFicheiro (fnome):
+    ata = []
+    f = open(fnome, 'r', encoding ='utf8')
+    data = json.load(f)
+    print(data)
+ 
+
+def criarLista (dic):
+    lista = []
+    #lista.append(dic['abstract'])
+    lista.append(dic['keywords'])
+    autores = []
+    for autor in dic['authors']:
+        autores.append((autor['name'], autor['affiliation']))
+    lista.append(autores)
+    lista.append(dic['doi'])
+    lista.append(dic['pdf'])
+    ano = int(dic['publish_date'].split("-")[0])
+    mes = int(dic['publish_date'].split("-")[1])
+    dia = int(dic['publish_date'].split("-")[2])
+    lista.append((ano, mes, dia))
+    lista.append(dic['title'])
+    lista.append(dic['url'])
+    return lista
+
+print(carregarFicheiro('ata_medica_papers.json'))
